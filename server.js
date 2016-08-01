6b330f132ea796e53cf97b52f9ffadf12ff8201e
Edward Huang <egunawan@uscd.edu>
2016-08-01 15:43:06 -0700

    fix server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/* deploying to heroku:
+    1. heroku create
+    2. git push heroku master
+    3. heroku open
+*/
 var express = require('express');
 
 // work the same but it is like a final, constant
@@ -13,7 +18,7 @@ app.use(function(req, res, next){ // next is when the middleware is done
   if(req.headers['x-forwarded-proto'] === 'http'){
     next(); // it will keep executing so return it to the browser
   } else {
-    res.redirect('https://' + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 
 });
