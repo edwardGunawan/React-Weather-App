c8d62a003c30b11f0cdbdb6c16959fb5569abac2
Edward Huang <egunawan@uscd.edu>
2016-08-03 11:30:28 -0700

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -18,8 +18,26 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3> About Components</h3>
-      <p> Welcome to the about page. </p>
+      <h1 className="text-center"> About </h1>
+      <p>
+        This app is a weather app in which user are able to see any weather
+        in any location.
+       </p>
+       <p>
+         Here are some of the tools I use:
+       </p>
+       <ul>
+         <li>
+           <a href="https://facebook.github.io/react/"> React</a> - This was
+             the javascript framework used.
+         </li>
+         <li>
+           <a href="http://openweathermap.org">Open weather map</a> - I used
+             open weather map to search for weather in the data.
+         </li>
+
+       </ul>
+
     </div>
   );
 }
